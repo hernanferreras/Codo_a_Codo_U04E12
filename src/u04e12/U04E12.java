d6b916e69550ca82aa3a8b52ed03329d914b1793
maxher73 <maxher73@yahoo.com.ar>
2020-03-23 22:29:44 -0300

    Unidad 4 Ejercicio 12 de la practica de Codo a Codo

---
+++ src/u04e12/U04E12.java
@@ -0,0 +1,26 @@
+
+package u04e12;
+
+/import java.util.Scanner;
+public class U04E12 {
+
+    public static void main(String[] args) {
+        char vocal;
+        String mensaje1 = "La letra ";
+        String mensaje2 = " es una vocal";
+        Scanner entrada=new Scanner(System.in);
+        System.out.print("Ingrese la letra: ");
+        vocal = entrada.next().charAt(0);
+        switch (vocal){
+            case 'a':
+            case 'e':
+            case 'i':
+            case 'o':
+            case 'u':
+                System.out.print(mensaje1+vocal+mensaje2);
+                break;
+            default:
+                System.out.print(mensaje1+vocal+" no"+mensaje2);
+        }
+    }
+}
